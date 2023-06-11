38a9c12cc22f71d7c9de42f34449d66ee57df33b
Deardevx <deardevx@gmail.com>
2023-06-11 22:06:58 +0700

    done

--- automate/main.py
+++ automate/main.py
@@ -1,70 +1,80 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import os
+
 
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-from selenium.common.exceptions import NoAlertPresentException
 
 import time 
 options = Options()
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options)
-driver.get("www")
+driver.get("https://fmr.nbtc.go.th/NBTCROS/Login.aspx")
 
-# #id
+#find box id pass
 user  = driver.find_element(By.ID, "UserName")
 password =  driver.find_element(By.ID, "Password")
+
 #login
 click_login = driver.find_element(By.ID, "bLogin")
 
 
-# #auto login
-user.send_keys("YourUsername")
+#send id password  login
+user.send_keys("puvakrint.p")
 time.sleep(3)
-password.send_keys("YourPassword")
+password.send_keys("Dearxcore555#@")
 time.sleep(3)
 click_login.click()
 
 # #click dms 
-#uToken("https://afm.nbtc.go.th/AFM","/DMS")
+
 click_dms =driver.find_element(By.XPATH,"/html/body/div/div/div/div[2]/a")
 
 time.sleep(3)
 click_dms.click()
 
-#choose Monitoring system
-monitoring_sysytem = driver.find_element(By.XPATH,"//a[@id='menu_station214708']")
-monitoring_sysytem.click()
 
 
-#process all pro 
+
+#-------process all pro--------
 
 #upload file 
-relative_path = "fco_data/test_9.csv"
+relative_path = "fco_data"
 working_directory = os.getcwd()
 absolute_path = os.path.join(working_directory, relative_path)
-upload_file  = driver.find_element(By.ID, "File1")
-print(absolute_path)
-upload_file.send_keys(f"{absolute_path}")
 
-#Click Preview 
-click_preview =  driver.find_element(By.XPATH,"//input[@id='bPreview']")
-time.sleep(3)
-click_preview.click()
+for file_name in os.listdir(absolute_path):
+    #choose Monitoring system
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,"//a[@id='menu_station214708']")))
+    monitoring_sysytem = driver.find_element(By.XPATH,"//a[@id='menu_station214708']")
+    monitoring_sysytem.click()
+    print(" Pick  ภภ.เขต 23 - A-23-nakhon ratchasima (Remote Station)")
+    
+    file_path = os.path.join(absolute_path,file_name)
 
-#click Save
-click_save = driver.find_element(By.XPATH,"//input[@id='bSave']")
-click_save.click()
+    upload_file  = driver.find_element(By.ID, "File1")
+    upload_file.send_keys(f"{file_path}")
+    print(f'upload: { file_path}')
+    #Click Preview 
+    click_preview =  driver.find_element(By.XPATH,"//input[@id='bPreview']")
+    time.sleep(3)
+    click_preview.click()
+    print("Click Preview")
+    #click Save
+    click_save = driver.find_element(By.XPATH,"//input[@id='bSave']")
+    click_save.click()
+    print("Click Save")
 
-
-#click done 
-# popup = driver.switch_to.alert
-# popup.accept()
-click_done = driver.find_element(By.XPATH,"/html/body/div[4]/div/div[10]/button[1]")
-click_done.click()
+    #click done 
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[4]/div/div[10]/button[1]")))
+    click_done = driver.find_element(By.XPATH,"/html/body/div[4]/div/div[10]/button[1]")
+    click_done.click()
+    print("Click Done ")
 
 
 
